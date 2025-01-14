1c63555a519ffa745d0ae972f415dbcf1dad90aa
mananuf <bmananaf@gmail.com>
2025-01-14 17:08:03 +0100

    adds merkle proof logic

--- main.go
+++ main.go
@@ -11,17 +11,32 @@ func main()  {
 		"kingsley",
 		"emmanuel",
 		"jethro",
-		"longs",
-		"lucky",
-		"dimka",
-		"scarface",
-		"manoah",
-		"victor",
+		// "longs",
+		// "lucky",
+		// "dimka",
+		// "scarface",
+		// "manoah",
+		// "victor",
 	}
 
-	merkleRoot := generateMerkleRootHash(transactions)
+	merkleTree := generateMerkleTree(transactions)
 
-	println("Merkle Root: ", merkleRoot)
+	for layer, hashes := range merkleTree {
+		println(layer, hashes)
+		println("====================================================>>>>>>>>>>>>>>>>>")
+	} 
+
+	root := getMerkleRootHash(merkleTree)
+	println("Merkle Root: ", root)
+
+	leaf := "0xa247681e6cdde53e207de4a7a39c2e4b36bcc8e794c74bcb8f841f8051639b24"
+	proofs := []string{
+		"0x575644babd48c4fa383ac90e01bef43631ccc45514a762e7394d56fc87c1f663",
+		"0x773d2b57449bafd0cf7ac96800a91ea65f25097a6d8b7ef5cbdfb307c76b1be0",
+	}
+
+	println(merkleProof(leaf, proofs, root, 0))
+
 }
 
 func hash(tx string) string {
@@ -42,9 +57,10 @@ func convertTransactionsToHash(transactions []string) []string {
 	return hashedTrasactions
 }
 
-func generateMerkleRootHash(transactions []string) string {
+func generateMerkleTree(transactions []string) []string {
 	hashedTransactions := convertTransactionsToHash(transactions)
 	transactionLength := len(hashedTransactions)
+	merkleTree := hashedTransactions;
 
 	for transactionLength > 1 {
 		// when the length of hashed Array is odd, duplicate the last hash to balance the hash pairing
@@ -58,10 +74,36 @@ func generateMerkleRootHash(transactions []string) string {
 			combinedHash := hash(hashedTransactions[index] + hashedTransactions[index + 1])
 			nextLayer = append(nextLayer, combinedHash)
 		}
-
+		println(nextLayer);
 		hashedTransactions = nextLayer
+		merkleTree = append(merkleTree, nextLayer...)
 		transactionLength = len(hashedTransactions)
 	}
 
-	return hashedTransactions[0]
+	return merkleTree
+}
+
+func merkleProof(
+	leafToVerify string,
+	proofs []string,
+	root string,
+	index uint64,
+) bool {
+	hashedVal := leafToVerify;
+	for _, proof := range proofs {
+		if (index % 2 == 0) {
+			hashedVal = hash(hashedVal + proof)
+		} else {
+			hashedVal = hash(proof + hashedVal)
+		}
+
+		index /= 2
+	}
+	println("output: ",hashedVal)
+	
+	return hashedVal == root
+}
+
+func getMerkleRootHash(merkleTree []string) string {
+	return merkleTree[len(merkleTree) - 1]
 }
